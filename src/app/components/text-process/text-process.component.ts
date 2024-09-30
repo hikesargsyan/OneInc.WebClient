a3e589e486f772c849ce2e0ff69485575684f80c
Hayk Sargsyan <dev.hikesargsian@gmail.com>
2024-09-30 10:21:52 +0400

    first commit

---
+++ src/app/components/text-process/text-process.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-process',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-process.component.html',
+  styleUrl: './text-process.component.scss'
+})
+export class TextProcessComponent {
+
+}
