2b3ec8da20fc31a44748583e317fe3d26940278c
Hayk Sargsyan <dev.hikesargsian@gmail.com>
2024-10-01 11:48:01 +0400

    updated text-process component

--- src/app/components/text-process/text-process.component.ts
+++ src/app/components/text-process/text-process.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Subscription, SubscriptionLike} from "rxjs";
+import {NgIf} from "@angular/common";
+import {UrlConstant} from "../../core/constants/url.constant";
+import {EventSourceService} from "../../providers/services/event-source.service";
 
 @Component({
   selector: 'app-text-process',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
+  providers: [EventSourceService],
   templateUrl: './text-process.component.html',
   styleUrl: './text-process.component.scss'
 })
-export class TextProcessComponent {
+export class TextProcessComponent implements OnDestroy {
+  textForm: FormGroup;
+  outputText: string = '';
+  isProcessing: boolean = false;
+  private eventSource!: EventSource;
+  private eventSourceSubscription!: SubscriptionLike;
+
+  constructor(private fb: FormBuilder, private eventSourceService: EventSourceService) {
+    this.textForm = this.fb.group({
+      inputText: ['', [ Validators.maxLength(100)]],
+    });
+  }
+
+
+  processText() {
+    if (this.textForm.invalid) {
+      return;
+    }
+
+    const inputText = this.textForm.get('inputText')?.value;
+
+    this.isProcessing = true;
+    this.outputText = '';
+
+    this.eventSourceSubscription = this.eventSourceService.processServerSentEvents(`/TextProcessing/${inputText}`)
+      .subscribe({
+          next: data => {
+            console.log(data);
+            this.outputText += data
+          },
+          error: err => {
+            this.isProcessing = false;
+          },
+          complete: () => {
+            this.isProcessing = false;
+          }
+        }
+      );
+  }
+
+  closeTextProcess() {
+    if (this.eventSourceSubscription) {
+      this.eventSourceSubscription.unsubscribe();
+    }
+    this.eventSourceService.close();
+    this.isProcessing = false;
+    this.outputText = '';
+  }
+
+  ngOnDestroy(): void {
+    this.closeTextProcess();
+  }
 
 }
+
+
+
